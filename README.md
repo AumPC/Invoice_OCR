2fae4be58c9533180848f2d67837d28c162e5a51
AumPC <blue-aum@live.com>
2019-07-23 19:11:23 +0700

    Change PDF to Image Tools - Solve OCR Image

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-#Invoice OCR
+# Invoice OCR
 Invoice OCR Project: To extract important data of Invoice and Import-Export Entry like Number, Consignee, Date, Price, Exchange Rate
 
 ### Requirement
 - Python 2.7+ or 3.x
-- PyPDF4 [Link] (https://github.com/claird/PyPDF4)
+- PDF2Image [Link] (https://pypi.org/project/pdf2image/)
 - Tesseract OCR [Link] (https://github.com/tesseract-ocr/tesseract)
 - PyTesseract [Link] (https://pypi.org/project/pytesseract/#description)
 
 ### Installation
-First, Install Tesseract OCR [Link] (https://github.com/tesseract-ocr/tesseract)
+First, 
+- Install Tesseract OCR [Link] (https://github.com/tesseract-ocr/tesseract)
+- Install Poppler : For Window [Link] (http://blog.alivate.com.au/poppler-windows/) and add `bin/` folder to `PATH`. , Linux: `sudo apt install poppler-utils`
 ```
 pip install pytesseract 
+pip install pdf2image 
 pip install pandas
 ```
 OR
--- ocr.py
+++ ocr.py
@@ -1,39 +1,45 @@
 import glob
-from PIL import Image
-import PyPDF4
+import cv2
 import pytesseract
-
-temp_image = "data/temp.png"
-
-def pdf_to_image(pdf_file):
-    pdf_data = PyPDF4.PdfFileReader(open(pdf_file, "rb")).getPage(0)
-    pdf_page = pdf_data['/Resources']['/XObject'].getObject()
-    for name_page in pdf_page:
-        page = pdf_page[name_page]
-        if page['/Subtype'] == '/Image':
-            size = (page['/Width'], page['/Height'])
-            data = page.getData()
-            mode = "RGB" if (page['/ColorSpace'] == '/DeviceRGB') else "P"
-            if page['/Filter'] == '/FlateDecode':
-                img = Image.frombytes(mode, size, data)
-                img.save(temp_image)
-            else:
-                img = open(temp_image, "wb")
-                img.write(data)
-                img.close()
-            print(temp_image)
-        break
+import pdf2image
 
 
-def get_ocr_text():
-    print(pytesseract.image_to_string(temp_image, lang='tha+eng'))
+def save_img(images, filename):
+    index = 1
+    filename = filename.split('\\')[-1].split('.')[0]
+    for image in images:
+        image.save("data/img/" + filename + "_" + str(index) + ".jpg")
+        index += 1
+
+
+def extract_invoice(data):
+    number = data.split('B.D.S')[1].split('\n')[0].strip().replace('.','')
+    consignee = data.split('Consignee')[1].split('\n')[0].replace(':','').strip()
+    price = data.split('TOTAL')[1].split('\n')[0].replace('CNF','').replace('()','').strip()
+    print(number)
+    print(consignee)
+    print(price)
+    return number, consignee, price
 
 
 if __name__ == '__main__':
-    path_pdf = "data/2016-June/"
+    # path_pdf = "data/300DPI/2016-June/"
+    path_pdf = "data/300DPI/2016-June-Invoice/"
     for pdf_file in glob.glob("D:\Project\Invoice_OCR/" + path_pdf + "*.pdf"):
-        pdf_to_image(pdf_file)
-        get_ocr_text()
-        # break
+        print("------------------------------------------------------------------------------------------------")
+        print(pdf_file)
 
+        images = pdf2image.convert_from_path(pdf_file, dpi=300, fmt="jpg")
+        # save_img(images, pdf_file)
 
+        for image in images:
+            data = pytesseract.image_to_string(image, lang='tha+eng')
+            if 'INVOICE' in data or'Invoice' in data or 'invoice' in data:
+                print("INVOICE")
+                extract_invoice(data)
+            else:
+                print("ENTRY")
+                # extract_entry(data)
+
+        # get_ocr_text()
+        print("------------------------------------------------------------------------------------------------")
