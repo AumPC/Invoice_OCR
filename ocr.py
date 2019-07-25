1ff434b78099ee353549674fb4ec508736d21560
AumPC <blue-aum@live.com>
2019-07-25 17:05:55 +0700

    Complete Invoice Extract

--- ocr.py
+++ ocr.py
@@ -29,9 +29,16 @@ def extract_invoice(data):
     date = date.split('\n')[0].strip()[1:].strip().split(' ')[0] if date is not None else None
     consignee = extract_data(data, ['Consignee', 'Gonsignee'])
     consignee = consignee.split('\n')[0].strip()[1:].strip() if consignee is not None else None
+    if consignee:
+        consignee_2 = extract_data(data, ['KHLONGSONG'])
+        consignee_2 = consignee_2.split('\n')[0].strip()[1:].strip() if consignee_2 is not None else None
+    else:
+        consignee_temp = extract_data(data, ['Consignee', 'Gonsignee'])
+        consignee = consignee_temp.split('\n')[2].strip() if consignee_temp is not None else None
+        consignee_2 = consignee_temp.split('\n')[3].strip() if consignee_temp is not None else None
     price = extract_data(data, ['TOTAL'])
     price = price.split('\n')[0].strip().split(' ')[-1] if price is not None else None
-    return {'number': number, 'invoice_date': date, 'consignee': consignee, 'price': price}
+    return {'number': number, 'invoice_date': date, 'consignee': consignee, 'consignee_2': consignee_2, 'price': price}
 
 
 def extract_entry(data):
@@ -40,11 +47,10 @@ def extract_entry(data):
 
 
 def open_csv(path_pdf):
-    print(path_project + "/result/" + path_pdf + ".csv")
     if os.path.isfile(path_project + "/result/" + path_pdf + ".csv"):
         csv = pd.read_csv(path_project + "/result/" + path_pdf + ".csv", index_col=0)    
     else:
-        csv = pd.DataFrame([], columns = ['number', 'invoice_date', 'consignee', 'price', 'entry_no', 'entry_date', 'exchange', 'out_unit', 'out_price', 'thai_price'])
+        csv = pd.DataFrame([], columns = ['number', 'invoice_date', 'consignee', 'consignee_2', 'price', 'entry_no', 'entry_date', 'exchange', 'out_unit', 'out_price', 'thai_price'])
         csv.set_index('number', inplace=True)
     return csv
 
@@ -55,7 +61,7 @@ def record_extract(csv, extract):
         extract.set_index('number', inplace=True)
         csv.update(extract)
     else:
-        column_list = ['number', 'invoice_date', 'consignee', 'price', 'entry_no', 'entry_date', 'exchange', 'out_unit', 'out_price', 'thai_price']
+        column_list = ['number', 'invoice_date', 'consignee', 'consignee_2', 'price', 'entry_no', 'entry_date', 'exchange', 'out_unit', 'out_price', 'thai_price']
         extract = { x: extract[x] if x in extract else None for x in column_list }
         extract = pd.DataFrame.from_records([extract])
         extract.set_index('number', inplace=True)
@@ -66,15 +72,18 @@ def record_extract(csv, extract):
 
 
 if __name__ == '__main__':
-    # path_pdf = "2016-June"
-    path_pdf = "2016-June-Invoice"
+    path_pdf = "2016-June"
+    # path_pdf = "2016-June-Invoice"
     csv = open_csv(path_pdf)
     for pdf_file in glob.glob(path_project + "/data/" + path_pdf + "/*.pdf"):
-        print("------------------------------------------------------------------------------------------------")
+        print("////////////////////////////////////////////////////////////////////////////////////////////////")
         print(pdf_file)
         images = pdf2image.convert_from_path(pdf_file, dpi=600, fmt="jpg")
+        count_page = 1
         # save_img(images, pdf_file)
         for image in images:
+            print(str(count_page) + " / " + str(len(images))) 
+            count_page += 1
             data = pytesseract.image_to_string(image, lang='tha+eng+osd')
             if 'INVOICE' in data or'Invoice' in data or 'invoice' in data:
                 data = pytesseract.image_to_string(image, lang='eng+osd')
@@ -86,6 +95,4 @@ if __name__ == '__main__':
                 extract_entry(data)
             else:
                 print("Not Found")
-        print("------------------------------------------------------------------------------------------------")
-
     csv.to_csv (path_project + "/result/" + path_pdf + ".csv", index=True, header=True)    
